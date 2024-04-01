45f15314d270b7cf9b7fbe3811348eebe084a274
Abraham Oliver <160440580+username121234@users.noreply.github.com>
2024-04-01 12:12:19 +0300

    Add files via upload

---
+++ My_list.py
@@ -0,0 +1,26 @@
+my_list=[]
+#print(type(my_list))
+my_list.extend([10,20,30,40])
+#print(my_list)
+my_list.insert(1, 15)
+#print(my_list)
+my_list.extend([50,60,70])
+#print(my_list)
+my_list.remove(70)
+#print(my_list)
+my_list.sort()
+#print(my_list)
+index=my_list.index(30)
+print(index)
+
+
+
+
+
+
+
+
+
+
+
+
